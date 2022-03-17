8c4cdc0336a6aef131a357ecba58d16d3bbab96f
borihalmgyi <72446213+borihalmgyi@users.noreply.github.com>
2022-03-17 09:07:03 +0100

    Add files via upload

---
+++ IKT nevezetességek.py
@@ -0,0 +1,16 @@
+print('\t1 Franciaország')
+print('\t2 Magyarország')
+print('\t3 Ausztrália')
+v = input("Add meg a választott menüpont sorszámát:")
+while v!='q':
+    if v == "1":
+        print('dog')
+
+    elif v == "2":
+        print('cat')
+
+    print('\t1  kutya angolul')
+    print('\t2  macska angolul')
+    print('\tq kilépés')
+    v = input("Add meg a választott menüpont sorszámát:")
+    
